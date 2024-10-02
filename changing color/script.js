68750f75bc18eb065c06876aa12580b0dbb149d3
Niranjan <codeniranjan31@gmail.com>
2024-10-02 22:54:19 +0530

    Add files via upload

---
+++ changing color/script.js
@@ -0,0 +1,21 @@
+
+const color_code="0123456789abcdef"
+
+let color=[];
+let real_color='red'
+console.log(real_color)
+real_color=null
+console.log(real_color)
+real_color='blue'
+console.log(real_color)
+// for (let  x = 0;  x < 6;  x++) {
+//     const random_number=Math.floor(Math.random()*16)
+//     const element = color_code[random_number]
+//     color.push(element)
+// }
+// const real_color=color.join('')
+// console.log(real_color)
+// console.log(typeof(real_color))
+
+// const randomNumber = Math.floor(Math.random() * 15);
+// console.log(randomNumber);
